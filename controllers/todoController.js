91ede1dc8d6d910823f89b62ea83a68c58e636e6
Amalia Putri Yulandi <amaliayulandi11@gmail.com>
2022-09-23 16:19:16 +0700

    build API findAll

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -4,6 +4,22 @@ const method = {}
 const Op = db.Sequelize.Op
 
 method.findAll = async (req, res) => {
+    try{
+        const name = req.query.name
+        var condition = name ? { todoName: {[Op.iLike]: `%${name}`}}: null
+
+        const data = await Todo.findAll({where: condition})
+
+        res.send({
+            statusCode: 200,
+            statusText: "Success",
+            message: "Your request for Get All data todo successfully",
+            data: data
+        })
+
+    }catch(error){
+        res.status(500).send(error)
+    }
 }
 
 method.create = async (req, res) => {
