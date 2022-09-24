79fa7938ac12642205b75b3d619986490ed6b169
Amalia Putri Yulandi <amaliayulandi11@gmail.com>
2022-09-24 17:10:19 +0700

    delete middleware

--- middlewares/schema.js
+++
@@ -1,9 +0,0 @@
-const Joi = require("joi")
-
-const schema = {
-    create: Joi.object({
-        todoName: Joi.string().require(),
-        todoDescription: Joi.string().require(),
-        todoIsComplete: Joi.boolean()
-    })
-}
