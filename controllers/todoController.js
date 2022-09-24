b989fb24a17cec1216a011251954da9f65a8201b
Amalia Putri Yulandi <amaliayulandi11@gmail.com>
2022-09-24 16:09:40 +0700

    build api update, delete and find one todo

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -21,6 +21,23 @@ method.findAll = async (req, res) => {
     }
 }
 
+method.findOne = async (req, res) => {
+    try{
+        const id = req.params.id
+        const data = await Todo.findOne({where:{id: id}})
+
+        res.send({
+            statusCode: 200,
+            statusText: "Success",
+            message: "Your request for Get One data todo successfully",
+            data: data
+        })
+
+    }catch(error){
+        res.status(500).send(error)
+    }
+}
+
 method.create = async (req, res) => {
     try{
 
@@ -51,19 +68,59 @@ method.create = async (req, res) => {
 
 method.update = async (req, res) => {
     try{
+        const id = req.params.id
 
+        let todoDetails = await Todo.findOne({where: {id: id}})
+
+        if(todoDetails == null){
+            res.status(404).json({
+                statusCode: 404,
+                statusText: "No Data Found",
+                message: "No Data Found from id Todo",
+             });
+        }else {
+            todoDetails = todoDetails.dataValues
+            todoDetails.todoIsComplete = req.body.todoIsComplete
+            const dataUpdate = await Todo.update(req.body, {where: {id:id} })
+    
+            res.send({
+                statusCode: 200,
+                statusText: "success",
+                message: "Your request for update todo successfully"
+            })
+        }
 
     }catch(error){
-
+        res.status(500).send(error)
     }
 }
 
 method.delete = async (req, res) => {
     try{
+        const id = req.params.id
+
+        let todoDetails = await Todo.findOne({where: {id: id}})
+
+        if(todoDetails == null){
+            res.status(404).json({
+                statusCode: 404,
+                statusText: "No Data Found",
+                message: "No Data Found from id Todo",
+             });
+        }else {
+            
+            await Todo.destroy({where: {id:id} })
+    
+            res.send({
+                statusCode: 200,
+                statusText: "success",
+                message: "Todo was deleted successfully!"
+            })
+        }
 
 
     }catch(error){
-
+        res.status(500).send(error)
     }
 }
 
--- routes/todoRoute.js
+++ routes/todoRoute.js
@@ -4,7 +4,10 @@ module.exports = app => {
     var router = require("express").Router();
 
     router.get("/", todo.findAll);
+    router.get("/:id", todo.findOne);
     router.post("/create", todo.create)
+    router.put("/update/:id", todo.update)
+    router.delete("/delete/:id", todo.delete)
     
     app.use('/api/todo', router);
 };
